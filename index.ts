5ee58e0c41bd486ebadea2e27d1b700506da63da
ArielGol <ariel00golubickas@gmail.com>
2022-12-01 15:37:15 -0300

    environment vars

--- index.ts
+++ index.ts
@@ -1,8 +1,23 @@
 import *as express from "express";
 
+
 const app=express();
 const PORT=3000;
+console.log("Mi nombre es:",process.env.NAME);
 
+//Ejemplo de uso de variable de entorno
+/*let user={
+  nombre:""
+}
+if(process.env.ENVIRONMENT==='develop'){
+  user.nombre="nombre random"
+}*/
+
+app.get("/env",(req,res)=>{
+  res.json({
+    environment:process.env.ENVIRONMENT
+  })
+})
 
 app.get("/hola",(req,res)=>{
   res.json({
